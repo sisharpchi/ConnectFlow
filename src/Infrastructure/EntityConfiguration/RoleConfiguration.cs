c7db2b81f4c26df276d4d62ed2251370481d2d98
Boy9999rich <boymurodmamarajapov1@gmail.com>
2025-06-12 20:42:20 +0500

    AddedEntityConfig

---
+++ src/Infrastructure/EntityConfiguration/RoleConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.EntityConfiguration;
+
+public class RoleConfiguration : IEntityTypeConfiguration<Role>
+{
+    public void Configure(EntityTypeBuilder<Role> builder)
+    {
+        builder.ToTable("Role");
+        builder.HasKey(b => b.Id);
+
+        builder.Property(b => b.Name)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Property(b => b.Description)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.HasMany(b => b.Users)
+            .WithOne(b => b.Role)
+            .HasForeignKey(b => b.RoleId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+}
