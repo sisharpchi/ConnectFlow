63cc390a4e6eac228019d295df1b71d5e8f01c6a
Boy9999rich <boymurodmamarajapov1@gmail.com>
2025-06-18 16:23:42 +0500

    RefreshTokenAndUserRoleAdded

--- src/Infrastructure/Configurations/RefreshTokenConfiguration.cs
+++ src/Infrastructure/Configurations/RefreshTokenConfiguration.cs
@@ -1,6 +1,29 @@
-﻿namespace Infrastructure.Configurations;
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-public class RefreshTokenConfiguration
+namespace Infrastructure.Configurations;
+
+public class RefreshTokenConfiguration : IEntityTypeConfiguration<RefreshToken>
 {
+    public void Configure(EntityTypeBuilder<RefreshToken> builder)
+    {
+        builder.HasKey(rt => rt.RefreshTokenId);
 
+        builder.Property(rt => rt.Token)
+            .IsRequired()
+            .HasMaxLength(500);
+
+        builder.Property(rt => rt.Expires)
+            .IsRequired();
+
+        builder.Property(rt => rt.IsRevoked)
+            .IsRequired();
+
+        builder.HasOne(rt => rt.User)
+            .WithMany(u => u.RefreshTokens)
+            .HasForeignKey(rt => rt.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
 }
+
