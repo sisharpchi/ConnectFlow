e00bf99e77d4f39bad875bbf4c27b2d331ea81ce
Boy9999rich <boymurodmamarajapov1@gmail.com>
2025-06-18 14:55:57 +0500

    Merge conflict resolved in UserRepository.cs and userConfiguration

--- src/Infrastructure/Configurations/UserConfiguration.cs
+++ src/Infrastructure/Configurations/UserConfiguration.cs
@@ -1,6 +1,45 @@
-﻿namespace Infrastructure.Configurations;
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-public class UserConfiguration
+namespace Infrastructure.Configurations;
+
+public class UserConfiguration : IEntityTypeConfiguration<User>
 {
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.HasKey(u => u.UserId);
 
+        builder.Property(u => u.FirstName)
+            .IsRequired()
+            .HasMaxLength(50);
+
+        builder.Property(u => u.LastName)
+            .IsRequired()
+            .HasMaxLength(50);
+
+        builder.Property(u => u.UserName)
+            .IsRequired()
+            .HasMaxLength(50);
+
+        builder.Property(u => u.Email)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Property(u => u.Password)
+            .IsRequired()
+            .HasMaxLength(256); // Assuming it's hashed
+
+        builder.Property(u => u.PhoneNumber)
+            .HasMaxLength(20);
+
+        builder.Property(u => u.Salt)
+            .IsRequired()
+            .HasMaxLength(128);
+
+        builder.HasOne(u => u.Role)
+            .WithMany(ur => ur.Users)
+            .HasForeignKey(u => u.UserRoleId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
 }
