7c10cc27656f239c65b2239ac3956dbe7a477d4a
Boy9999rich <boymurodmamarajapov1@gmail.com>
2025-06-19 19:16:08 +0500

    RefreshTokenAndCoreExceptionsAdded

--- src/Infrastructure/Persistence/Repositories/RefreshTokenRepository.cs
+++ src/Infrastructure/Persistence/Repositories/RefreshTokenRepository.cs
@@ -1,27 +1,45 @@
 using Application.RepositoryContracts;
+using Core.Errors;
 using Domain.Entities;
-
+using Microsoft.EntityFrameworkCore;
 namespace Infrastructure.Persistence.Repositories;
 
 public class RefreshTokenRepository(AppDbContext appDbContext) : IRefreshTokenRepository
-{
-    public Task InsertRefreshTokenAsync(RefreshToken refreshToken)
+{ 
+    public async Task InsertRefreshTokenAsync(RefreshToken refreshToken)
     {
-        throw new NotImplementedException();
+        await appDbContext.RefreshTokens.AddAsync(refreshToken);
+        await appDbContext.SaveChangesAsync();
     }
 
-    public Task RemoveRefreshTokenAsync(string token)
+    public async Task<RefreshToken?> SelectActiveTokenByUserIdAsync(long userId)
     {
-        throw new NotImplementedException();
+        RefreshToken? refreshToke;
+        try
+        {
+            refreshToke = await appDbContext.RefreshTokens
+            .Include(rf => rf.User)
+            .SingleOrDefaultAsync(rf => rf.UserId == userId && rf.IsRevoked == false && rf.Expires > DateTime.UtcNow);
+        }
+        catch (InvalidOperationException ex)
+        {
+            throw new DuplicateEntryException($"2 or more active refreshToken found with userId: {userId} found!\nAnd {ex.Message}");
+        }
+        return refreshToke;
     }
 
-    public Task<RefreshToken?> SelectActiveTokenByUserIdAsync(long userId)
+    public async Task<RefreshToken> SelectRefreshTokenAsync(string refreshToken, long userId)
     {
-        throw new NotImplementedException();
+        var refToken = await appDbContext.RefreshTokens.FirstOrDefaultAsync(rt => rt.Token == refreshToken && rt.UserId == userId);
+        return refToken ?? throw new InvalidArgumentException($"RefreshToken with {userId} is invalid");
     }
-
-    public Task<RefreshToken> SelectRefreshTokenAsync(string refreshToken, long userId)
+    public async Task RemoveRefreshTokenAsync(string token)
     {
-        throw new NotImplementedException();
+        var refreshToken = await appDbContext.RefreshTokens.FirstOrDefaultAsync(rf => rf.Token == token);
+        if (refreshToken == null) throw new EntityNotFoundException($"Refresh token: {refreshToken} not found");
+
+        appDbContext.RefreshTokens.Remove(refreshToken);
+        await appDbContext.SaveChangesAsync();
     }
 }
+
