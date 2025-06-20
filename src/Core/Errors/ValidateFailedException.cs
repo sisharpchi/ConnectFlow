e8a69c4470cedb85038d266d9d0b3286527b2b83
Boy9999rich <boymurodmamarajapov1@gmail.com>
2025-06-20 19:17:44 +0500

    oneExceptionAdded

--- src/Core/Errors/ValidateFailedException.cs
+++ src/Core/Errors/ValidateFailedException.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Core.Errors
 {
-    internal class ValidateFailedException
+    public class ValidateFailedException : BaseException
     {
+        public ValidateFailedException() { }
+        public ValidateFailedException(String message) : base(message) { }
+        public ValidateFailedException(String message, Exception inner) : base(message, inner) { }
+        protected ValidateFailedException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+
     }
 }
