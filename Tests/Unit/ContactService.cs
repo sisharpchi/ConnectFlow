9b83a44dcc0b2358ce0d5787e29b78770bce043d
Boy9999rich <boymurodmamarajapov1@gmail.com>
2025-06-20 22:00:47 +0500

    UnitTests are Added

--- Tests/Unit/ContactService.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Tests.Unit;
-
-public class ContactService
-{
-
-}
---
+++ Tests/Unit/ContactServiceTests.cs
@@ -0,0 +1,16 @@
+﻿using Application.RepositoryContracts;
+using Moq;
+
+namespace Tests.Unit;
+
+public class ContactServiceTests
+{
+    private readonly Mock<IContactRepository> _mockRepo;
+    private readonly ContactServiceTests _service;
+
+    public ContactServiceTests()
+    {
+        _mockRepo = new Mock<IContactRepository>();
+        _service = new ContactService(_mockRepo.Object);
+    }
+}
